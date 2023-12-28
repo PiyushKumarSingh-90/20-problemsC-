a5ffa5d66be8e6c9538cda258366a6fcc3f75cb1
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2023-12-28 11:35:07 +0530

    Add files via upload

---
+++ 21_MajorityElement.cpp
@@ -0,0 +1,32 @@
+// Write a program to find the majority element in an array (element appearing more than N/2 times).
+
+#include <iostream>
+
+int findMajorityElement(int arr[], int size) {
+    int majorityElement = arr[0];
+    int count = 1;
+
+    for (int i = 1; i < size; ++i) {
+        if (count == 0) {
+            majorityElement = arr[i];
+            count = 1;
+        } else if (arr[i] == majorityElement) {
+            ++count;
+        } else {
+            --count;
+        }
+    }
+
+    return majorityElement;
+}
+
+int main() 
+{
+    int arr[] = {2, 2, 1, 1, 1, 2, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int majorityElement = findMajorityElement(arr, size);
+
+    std::cout << "Majority Element: " << majorityElement << std::endl;
+
+    return 0;
+}
